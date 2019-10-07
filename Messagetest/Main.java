3f359fe1e64b1b9abee60f8988f486f173205f1a
jordey van zelst <jordey@gmail.com>
2019-10-07 18:59:37 +0200

    basic setup

---
+++ Messagetest/Main.java
@@ -0,0 +1,36 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	/*
+	 * TSpecify communication ports for miner and messager
+	 * Change port no. if you get error like Bind Problem: Address already in use
+	 * Users and miner should be connected to same Wi-Fi Network
+	 */
+	public static final int port = 1111;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+		primaryStage.setTitle("WISPR chat");
+		primaryStage.setScene(new Scene(root, 650, 350));
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) throws Exception {
+		//Miner Mode
+		/*
+		Miner pro = new Miner("pro",port);
+		pro.start();
+		*/
+
+		//User Mode
+		launch(args);
+		System.exit(0);
+
+	}
+}
